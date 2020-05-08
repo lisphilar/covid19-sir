1bd54c559c3ecbbc832ae95e85b327225d5fe34a
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2020-05-09 00:31:08 +0900

    add: plotting an d map

---
+++ covsirphy/util/map.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib
+import japanmap
+import pandas as pd
+
+
+def jpn_map(prefectures, values, title, cmap_name="Reds"):
+    """
+    Show colored Japan prefecture map.
+    @prefectures <list/pd.Series[str]>: prefecture name.
+    @values <int/float>: value of each prefectures
+    @title <str>: title of the figure
+    @cmap_name <str>: Please refere to
+        - https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html
+    """
+    # Prefecture code created in
+    # https://www.kaggle.com/lisphilar/eda-of-japan-dataset
+    # Primary data: http://nlftp.mlit.go.jp/ksj/gml/codelist/PrefCd.html
+    # cf. https://www.japanvisitor.com/japan-travel/prefectures-map
+    pref_code_dict = dict(
+        [
+            ('Hokkaido', 1), ('Aomori', 2), ('Iwate', 3),
+            ('Miyagi', 4), ('Akita', 5), ('Yamagata', 6),
+            ('Fukushima', 7), ('Ibaraki', 8), ('Tochigi', 9),
+            ('Gunma', 10), ('Saitama', 11), ('Chiba', 12),
+            ('Tokyo', 13), ('Kanagawa', 14), ('Niigata', 15),
+            ('Toyama', 16), ('Ishikawa', 17), ('Fukui', 18),
+            ('Yamanashi', 19), ('Nagano', 20), ('Gifu', 21),
+            ('Shizuoka', 22), ('Aichi', 23), ('Mie', 24),
+            ('Shiga', 25), ('Kyoto', 26), ('Osaka', 27), ('Hyogo', 28),
+            ('Nara', 29), ('Wakayama', 30), ('Tottori', 31),
+            ('Shimane', 32), ('Okayama', 33), ('Hiroshima', 34),
+            ('Yamaguchi', 35), ('Tokushima', 36), ('Kagawa', 37),
+            ('Ehime', 38), ('Kochi', 39), ('Fukuoka', 40),
+            ('Saga', 41), ('Nagasaki', 42), ('Kumamoto', 43),
+            ('Oita', 44), ('Miyazaki', 45), ('Kagoshima', 46),
+            ('Okinawa', 47)
+        ]
+    )
+    # Data to dataframe
+    df = pd.DataFrame({"Name": prefectures, "Value": values})
+    df["Code"] = df["Name"].map(pref_code_dict)
+    df["Japanese_name"] = df["Code"].apply(lambda x: japanmap.pref_names[x])
+    df = df.set_index("Japanese_name")
+    # Color code
+    cmap = cm.get_cmap(cmap_name)
+    norm = matplotlib.colors.Normalize(
+        vmin=df["Value"].min(), vmax=df["Value"].max())
+
+    def fcol(x): return "#" + bytes(cmap(norm(x), bytes=True)[:3]).hex()
+    # Show figure
+    plt.xticks(color="None")
+    plt.yticks(color="None")
+    plt.tick_params(length=0)
+    plt.imshow(japanmap.picture(df["Value"].apply(fcol)))
+    mappable = cm.ScalarMappable(norm=norm, cmap=cmap)
+    mappable._A = []
+    plt.colorbar(mappable)
+    plt.title(title)
+    plt.show()
---
+++ covsirphy/util/plotting.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.ticker import ScalarFormatter
+
+plt.style.use("seaborn-ticks")
+plt.rcParams["xtick.direction"] = "in"
+plt.rcParams["ytick.direction"] = "in"
+plt.rcParams["font.size"] = 11.0
+plt.rcParams["figure.figsize"] = (9, 6)
+
+
+def line_plot(df, title, xlabel=None, ylabel="Cases",
+              h=None, v=None, xlim=(None, None), ylim=(0, None),
+              math_scale=True, x_logscale=False, y_logscale=False, y_integer=False,
+              show_legend=True, bbox_to_anchor=(1.02, 0),  bbox_loc="lower left"):
+    """
+    Show chlonological change of the data.
+    """
+    ax = df.plot()
+    # Scale
+    if math_scale:
+        ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax.ticklabel_format(style="sci",  axis="y", scilimits=(0, 0))
+    if x_logscale:
+        ax.set_xscale("log")
+        if xlim[0] == 0:
+            xlim = (None, None)
+    if y_logscale:
+        ax.set_yscale("log")
+        if ylim[0] == 0:
+            ylim = (None, None)
+    if y_integer:
+        fmt = matplotlib.ticker.ScalarFormatter(useOffset=False)
+        fmt.set_scientific(False)
+        ax.yaxis.set_major_formatter(fmt)
+    # Set metadata of figure
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_xlim(*xlim)
+    ax.set_ylim(*ylim)
+    if show_legend:
+        ax.legend(bbox_to_anchor=bbox_to_anchor, loc=bbox_loc, borderaxespad=0)
+    else:
+        ax.legend().set_visible(False)
+    if h is not None:
+        ax.axhline(y=h, color="black", linestyle=":")
+    if v is not None:
+        if not isinstance(v, list):
+            v = [v]
+        for value in v:
+            ax.axvline(x=value, color="black", linestyle=":")
+    plt.tight_layout()
+    plt.show()
