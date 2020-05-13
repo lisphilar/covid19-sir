19094de3475df5eef940e98a373c888e0f595120
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2020-05-13 19:54:14 +0900

    add: word definition

---
+++ covsirphy/cleaning/word.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Word(object):
+    """
+    Word definition.
+    """
+    # Variables of SIR-like model
+    S = "Susceptible"
+    C = "Confirmed"
+    CI = "Infected"
+    F = "Fatal"
+    R = "Recovered"
+    # Column names
+    DATE = "Date"
+    T = "Elapsed"
+    COUNTRY = "Country"
+    PROVINCE = "Province"
+    COLUMNS = [DATE, COUNTRY, PROVINCE, C, CI, F, R]
+    VALUE_COLUMNS = [C, CI, F, R]
+    # EDA
+    RATE_COLUMNS = [
+        "Fatal per Confirmed",
+        "Recovered per Confirmed",
+        "Fatal per (Fatal or Recovered)"
+    ]
