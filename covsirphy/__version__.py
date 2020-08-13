7dfc19e5a6f7d85b60c077ae5fe7cc37e038ac7d
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2020-08-14 02:31:47 +0900

    version 2.6.0-eta

--- covsirphy/__version__.py
+++ covsirphy/__version__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "2.6.0-zeta.new.72.fix.120.155.157.163.164.165.174"
+__version__ = "2.6.0-eta.new.72.167.fix.120.155.157.163.164.165.174"
