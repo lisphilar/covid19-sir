8b79e8f129f7f06ca4fa3aff04950059efbbb47d
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2020-11-01 18:03:19 +0900

    example

--- example/model_validation.py
+++ example/model_validation.py
@@ -14,11 +14,41 @@ def main():
     output_dir.mkdir(exist_ok=True, parents=True)
     # Setting
     validator = cs.ModelValidator(n_trials=8, seed=1)
-    # Execute validation
-    validator.run(cs.SIR, timeout=60, allowance=(0.98, 1.02))
-    validator.run(cs.SIRD, timeout=60, allowance=(0.98, 1.02))
-    validator.run(cs.SIRF, timeout=60, allowance=(0.98, 1.02))
+    # Execute validation with default setting (60 sec, 0.98, 1.02)
+    validator.run(cs.SIR)
+    validator.run(cs.SIRD)
+    validator.run(cs.SIRF)
     print(validator.summary())
+    save_df(
+        validator.summary(), name="default.csv", country="summary", use_index=False)
+    # Execute validation with long timeout and default allowance setting (0.98, 1.02)
+    validator.run(cs.SIR, timeout=90)
+    validator.run(cs.SIRD, timeout=90)
+    validator.run(cs.SIRF, timeout=90)
+    print(validator.summary())
+    save_df(
+        validator.summary(), name="long.csv", country="summary", use_index=False)
+    # Execute validation with long timeout and restricted allowance
+    validator.run(cs.SIR, timeout=90, allowance=(0.99, 1.01))
+    validator.run(cs.SIRD, timeout=90, allowance=(0.99, 1.01))
+    validator.run(cs.SIRF, timeout=90, allowance=(0.99, 1.01))
+    print(validator.summary())
+    save_df(
+        validator.summary(), name="long_restricted.csv", country="summary", use_index=False)
+
+
+def save_df(df, name, output_dir, country, use_index=True):
+    """
+    Save the dataframe as a CSV file.
+
+    Args:
+        df (pandas.DataFrame): dataframe
+        name (str): name of the dataframe
+        output_dir (pathlib.Path): path of the directory to save the figure
+        country (str): country name or abbr
+        use_index (bool): if True, include index
+    """
+    df.to_csv(output_dir.joinpath(f"{name}.csv"), index=use_index)
 
 
 if __name__ == "__main__":
