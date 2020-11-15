45c1d103cfeff48e18b67657902b96eaed84af5e
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2020-11-16 00:39:57 +0900

    example

---
+++ example/load_linelist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+import warnings
+import covsirphy as cs
+
+
+def main():
+    warnings.simplefilter("error")
+    # Create output directory in example directory
+    code_path = Path(__file__)
+    input_dir = code_path.parent.with_name("input")
+    output_dir = code_path.with_name("output").joinpath(code_path.stem)
+    output_dir.mkdir(exist_ok=True, parents=True)
+    # Create data loader instance
+    data_loader = cs.DataLoader(input_dir)
+    # Load Linelist of case reports
+    linelist_data = data_loader.linelist()
+    print(linelist_data.citation)
+    linelist_data.cleaned().to_csv(
+        output_dir.joinpath("linelist_cleaned.csv"), index=False)
+
+
+if __name__ == "__main__":
+    main()
