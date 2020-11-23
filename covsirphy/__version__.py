877f6394f58c382bd45156dd41dcbd1c53782643
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2020-11-23 22:55:59 +0900

    version 2.11.0-kappa

--- covsirphy/__version__.py
+++ covsirphy/__version__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "2.11.0-iota.new.281.334.340.351.fixing.339.refactor.340"
+__version__ = "2.11.0-kappa.new.281.334.340.351.fixing.339.refactor.354"
