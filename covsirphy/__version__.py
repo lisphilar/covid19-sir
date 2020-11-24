06bc4a0570f3645d77b78ab5d2b6a9cb92a18532
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2020-11-25 01:39:28 +0900

    version 2.11.0-lambda

--- covsirphy/__version__.py
+++ covsirphy/__version__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "2.11.0-kappa.new.281.334.340.351.fixing.339.refactor.354"
+__version__ = "2.11.0-lambda.new.281.334.340.351.356.fixing.339.refactor.354"
