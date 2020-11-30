387859bf75e3841fc58588aa201e5777653e7c34
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2020-11-30 21:42:08 +0900

    test: update fixture of ParamTracker

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,18 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from covsirphy import DataLoader, PhaseSeries, ParamTracker
+from covsirphy import DataLoader
+
+
+@pytest.fixture(scope="session")
+def data_loader():
+    return DataLoader()
 
 
 @pytest.fixture(scope="session")
 def jhu_data():
     data_loader = DataLoader()
     return data_loader.jhu()
-
-
-@pytest.fixture(scope="session")
-def data_loader():
-    return DataLoader()
 
 
 @pytest.fixture(scope="session")
@@ -32,16 +32,3 @@ def oxcgrt_data():
 def japan_data():
     data_loader = DataLoader()
     return data_loader.japan()
-
-
-@pytest.fixture(scope="session")
-def param_tracker():
-    data_loader = DataLoader()
-    jhu_data = data_loader.jhu()
-    population_data = data_loader.population()
-    population = population_data.value(country="Japan")
-    record_df = jhu_data.subset(country="Japan", population=population)
-    series = PhaseSeries("01Apr2020", "01Nov2020", population)
-    return ParamTracker(
-        record_df=record_df, phase_series=series, area="Japan"
-    )
