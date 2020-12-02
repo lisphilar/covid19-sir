c6d171c0c45d75718c31b3ae3cecdaeb67e844da
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2020-12-03 00:40:02 +0900

    version 2.12.0-iota

--- covsirphy/__version__.py
+++ covsirphy/__version__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "2.12.0-theta.new.362.366.367.fix.360.373.solving.357"
+__version__ = "2.12.0-iota.new.362.366.367.fix.360.373.solving.357.359"
