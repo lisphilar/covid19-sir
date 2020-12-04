7f948711197439e529396c9753aa365d62b50f84
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2020-12-05 00:17:37 +0900

    version 2.12.0-mu

--- covsirphy/__version__.py
+++ covsirphy/__version__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "2.12.0-lambda.new.362.366.367.fix.360.373.solving.357.359"
+__version__ = "2.12.0-mu.new.359.362.366.367.fix.360.373.solving.357"
