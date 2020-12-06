b73e402ae4746992c18b332be004a671c165f570
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2020-12-06 18:42:20 +0900

    version 2.12.0-omicron

--- covsirphy/__version__.py
+++ covsirphy/__version__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "2.12.0-xi.new.339.357.359.362.366.367.fix.360.373.solving"
+__version__ = "2.12.0-omicron.new.339.357.359.362.366.367.fix.360.373.solving.383"
