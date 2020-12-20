f13a433feceb5e583ab338de0ae6fc4fcbeb3310
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2020-12-20 23:53:00 +0900

    update: filling in subsetting

--- covsirphy/cleaning/vaccine_data.py
+++ covsirphy/cleaning/vaccine_data.py
@@ -129,7 +129,8 @@ class VaccineData(CleaningBase):
         # Resampling
         df = df.set_index(self.DATE).resample("D").sum()
         # Fill in the blanks
-        df = df.fillna(method="ffill")
+        df[self.VAC] = df[self.VAC].replace(
+            0, None).fillna(method="ffill").fillna(0)
         # Check records were found
         if df.empty:
             raise SubsetNotFoundError(
