10590abd657384897f11b21a35757bcbd3030a88
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2021-01-09 22:46:28 +0900

    Revert "add: Scenario.history() support actual confirmed"
    This reverts commit 044c8ff42e8a1b10117dfb5b4d7533c88e4d1234.

--- covsirphy/analysis/scenario.py
+++ covsirphy/analysis/scenario.py
@@ -811,7 +811,7 @@ class Scenario(DataHandler):
             df.insert(0, self.SERIES, name)
             append(df)
         if with_actual:
-            df = self.record_df.copy()
+            df = self.records(show_figure=False)
             df.insert(0, self.SERIES, self.ACTUAL)
             append(df)
         return pd.concat(dataframes, axis=0, sort=False)
