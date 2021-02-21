be880acfcf39a00818a644cdf94ac86711b8fe93
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2021-02-21 19:32:49 +0900

    test

--- tests/test_scenario.py
+++ tests/test_scenario.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import warnings
 import pytest
 from covsirphy import ScenarioNotFoundError, UnExecutedError, NotInteractiveError
-from covsirphy import Scenario, DataHandler
+from covsirphy import Scenario, DataHandler, PhaseSeries
 from covsirphy import Term, SIRF
 
 
@@ -86,3 +86,46 @@ class TestScenario(object):
         snl.show_complement()
         snl.records()
         snl.records_diff()
+
+    def test_register_scenario(self, snl):
+        with pytest.raises(ScenarioNotFoundError):
+            snl.clear(name="New", include_past=True, template="Un-registered")
+        snl.clear(name="New", include_past=True, template="Main")
+        assert isinstance(snl["New"], PhaseSeries)
+        snl.delete(name="New")
+        with pytest.raises(ScenarioNotFoundError):
+            snl["New"]
+
+    def test_add_delete(self, snl):
+        # Add phases
+        snl.add(end_date="01Sep2020")
+        with pytest.raises(ValueError):
+            snl.add(end_date="01Jun2020")
+        # Deprecated
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        snl.add_phase(end_date="01Oct2020")
+        # Add a phase with days
+        snl.add(days=10)
+        # Add a phase to the last date
+        snl.add()
+        # Delete phases
+        snl.delete(phases=["last"])
+        snl.clear(include_past=True)
+
+    def test_enable(self, snl):
+        # Setting
+        snl.add(end_date="01May2020")
+        snl.add(end_date="01Sep2020")
+        snl.add(end_date="01Nov2020")
+        # Disable/enable
+        snl.disable(phases=["1st", "2nd"])
+        snl.enable(phases=["2nd"])
+        # Test (only 1st phase is disabled)
+        assert "0th" in snl.summary().index
+        assert "1st" not in snl.summary().index
+        assert "2nd" in snl.summary().index
+        # Clear all phases
+        snl.clear(include_past=True)
+
+    def test_trend(self, snl):
+        snl.trend()
