81c0b07252aa3efd0a5b0f68bfa07fb207f3e7c7
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2021-02-28 20:04:02 +0900

    test

--- tests/test_change_finder.py
+++ tests/test_change_finder.py
@@ -15,7 +15,7 @@ class TestChangeFinder(object):
         ]
     )
     def test_find(self, jhu_data, population_data, country, max_rmsle=20.0):
-        warnings.filterwarnings("error")
+        warnings.filterwarnings("error", category=DeprecationWarning)
         # Setup
         population = population_data.value(country)
         sr_df = jhu_data.to_sr(country=country, population=population)
