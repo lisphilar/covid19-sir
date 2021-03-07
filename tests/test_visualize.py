4b69f38f5bf0f992a3732930df15b0c500d56f27
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2021-03-07 23:00:46 +0900

    test

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -7,8 +7,8 @@ import matplotlib
 import pandas as pd
 import pytest
 from covsirphy import VisualizeBase, ColoredMap, LinePlot
+from covsirphy import Term, UnExecutedError
 from covsirphy import jpn_map
-from covsirphy import Term
 
 
 class TestVisualizeBase(object):
@@ -145,3 +145,12 @@ class TestLinePlot(object):
             lp.plot(data=df)
             lp.y_axis(y_integer=True)
             lp.line(h=100_000)
+
+    def test_legend(self, jhu_data, imgfile):
+        df = jhu_data.subset(country="Japan").set_index(Term.DATE)
+        with LinePlot(filename=imgfile) as lp:
+            with pytest.raises(UnExecutedError):
+                lp.legend()
+            lp.plot(data=df)
+            lp.legend_hide()
+            lp.legend()
