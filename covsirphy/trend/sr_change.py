c0c1a8b3e9308f0d35e1c73eae4ce2b93d31b896
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2021-03-14 15:10:24 +0900

    fix: dotted lines not shown

--- covsirphy/trend/sr_change.py
+++ covsirphy/trend/sr_change.py
@@ -147,13 +147,13 @@ class _SRChange(Term):
             kwargs: keyword arguments of covsirphy.trend_plot()
         """
         # Title
-        if len(change_points) == 1:
-            title = f"{area}: S-R trend without change points"
-        else:
+        if len(change_points):
             _list = [date.strftime(self.DATE_FORMAT) for date in change_points]
             strings = [", ".join(_list[i: i + 6]) for i in range(0, len(_list), 6)]
             change_str = ",\n".join(strings)
             title = f"{area}: S-R trend changed on\n{change_str}"
+        else:
+            title = f"{area}: S-R trend without change points"
         # Curve fitting
         fit_df = self._fitting(change_points)
         # Show S-R plane
@@ -164,6 +164,14 @@ class _SRChange(Term):
             "xlabel": self.R,
             "ylabel": self.S,
             "show_legend": True,
+            "v": [fit_df.loc[point, self.R] for point in change_points],
         }
         plot_kwargs.update(kwargs)
-        trend_plot(df=fit_df.set_index(self.R), title=title, actual_col=self.ACTUAL, **kwargs)
+        trend_plot(df=fit_df.set_index(self.R), actual_col=self.ACTUAL, **plot_kwargs)
+
+
+class Trend(object):
+    """
+    Deprecated. Please use TrendDetector class.
+    """
+    pass
