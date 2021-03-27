9e528a8cb72e8ab3e480efac4fab8da8427fe9a5
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2021-03-28 00:47:03 +0900

    update: type to ensure

--- covsirphy/util/term.py
+++ covsirphy/util/term.py
@@ -379,7 +379,7 @@ class Term(object):
 
         Args:
             target (list[object]): target to ensure
-            candidates (list[string] or None): list of candidates, if we have
+            candidates (list[object] or None): list of candidates, if we have
             name (str): argument name of the target
 
         Returns:
@@ -392,15 +392,15 @@ class Term(object):
             return target
         # Check the target is a sub-list of candidates
         try:
-            candidate_str = ", ".join(candidates)
+            strings = [candidate for candidate in candidates]
+            candidate_str = ", ".join(strings)
         except TypeError:
             raise TypeError(
-                f"@candidates must be a list strings, but {candidates} was applied.") from None
-        ok_list = [str(element) in candidates for element in target]
+                f"@candidates must be a list, but {candidates} was applied.") from None
+        ok_list = [element in candidates for element in target]
         if all(ok_list):
             return target
-        raise KeyError(
-            f"@{name} must be a sub-list of [{candidate_str}], but {target} was applied.") from None
+        raise KeyError(f"@{name} must be a sub-list of [{candidate_str}], but {target} was applied.") from None
 
     @classmethod
     def divisors(cls, value):
