fa89996c8df91160b1c7320f567cfb51b4451b71
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2021-03-29 23:12:34 +0900

    docs: update example script of ModelValidator

--- example/model_validation.py
+++ example/model_validation.py
@@ -12,7 +12,7 @@ import covsirphy as cs
 
 
 def main():
-    # This script works with version >= 2.17.0-eta
+    # This script works with version >= 2.18.0-alpha
     print(cs.get_version())
     # Create output directory in example directory
     code_path = Path(__file__)
@@ -21,11 +21,13 @@ def main():
     filer = cs.Filer(output_dir, numbering="01")
     # Setting
     models = [cs.SIR, cs.SIRD, cs.SIRF]
+    step_numbers = list(range(3, 10))
     # Execute validation
-    validator = cs.ModelValidator(n_trials=8, seed=1)
-    for model in models:
-        validator.run(model)
-    validator.summary().to_csv(**filer.csv("summary", index=False))
+    for step_n in step_numbers:
+        validator = cs.ModelValidator(tau=1440, n_trials=8, step_n=step_n, seed=0)
+        for model in models:
+            validator.run(model)
+        validator.summary().to_csv(**filer.csv(f"summary_{step_n}-points", index=False))
 
 
 if __name__ == "__main__":
