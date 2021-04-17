c27abafbe7ca1efe3c69cc18a22dd509d2818f90
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2021-04-18 01:11:59 +0900

    test

--- tests/test_ode.py
+++ tests/test_ode.py
@@ -20,6 +20,23 @@ class TestODEHandler(object):
         sim_df = handler.simulate().set_index(Term.DATE)
         assert sim_df.index.min() == pd.to_datetime(start_date)
         assert sim_df.index.max() == pd.to_datetime("28Feb2021")
+        assert set(sim_df.reset_index().columns) == set(Term.DSIFR_COLUMNS)
+
+    @pytest.mark.parametrize("model", [SIR, SIRD, SIRF])
+    @pytest.mark.parametrize("start_date", ["01Jan2021"])
+    @pytest.mark.parametrize("tau", [720])
+    def test_estimate_example(self, model, start_date, tau):
+        # Simulation
+        y0_dict = model.EXAMPLE["y0_dict"]
+        param_dict = model.EXAMPLE["param_dict"]
+        sim_handler = ODEHandler(model, start_date, tau)
+        sim_handler.add(end_date="31Jan2021", y0_dict=y0_dict, param_dict=param_dict)
+        # sim_handler.add(end_date="28Feb2021", y0_dict=None, param_dict=param_dict)
+        sim_df = sim_handler.simulate()
+        # Parameter estimation
+        handler = ODEHandler(model, start_date, tau)
+        print(handler.estimate_parameters(sim_df, metric="RMSLE"))
+        raise NotImplementedError
 
 
 class TestODE(object):
