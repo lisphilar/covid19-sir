9b7bc20164b49fa27b205d24d58026b46a6c59b3
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2021-04-29 00:16:27 +0900

    add: PhaseTracker.summary()

--- covsirphy/analysis/phase_tracker.py
+++ covsirphy/analysis/phase_tracker.py
@@ -53,14 +53,16 @@ class PhaseTracker(Term):
         if start <= self._today:
             self._track_df.loc[start:min(self._today, end), self.ID] = self._track_df[self.ID].max() + 1
         # Add a future phase (tomorrow -> end)
-        if end <= self._today:
-            return self
-        df = pd.DataFrame(
-            index=pd.date_range(self._today + timedelta(days=1), end),
-            columns=self._track_df.columns)
-        df.index.name = self.DATE
-        df[self.ID] = self._track_df[self.ID].max() + 1
-        self._track_df = pd.concat([self._track_df, df], axis=0).resample("D").last()
+        if self._today < end:
+            phase_start = max(self._today + timedelta(days=1), start)
+            df = pd.DataFrame(
+                index=pd.date_range(phase_start, end), columns=self._track_df.columns)
+            df.index.name = self.DATE
+            df[self.ID] = self._track_df[self.ID].max() + 1
+            self._track_df = pd.concat([self._track_df, df], axis=0).resample("D").last()
+        # Fill in blanks
+        series = self._track_df[self.ID].copy()
+        self._track_df.loc[(series.index <= end) & (series == 0), self.ID] = series.max() + 1
         return self
 
     def track(self):
@@ -70,7 +72,7 @@ class PhaseTracker(Term):
         Returns:
             pandas.DataFrame
                 Index
-                    str: phase names
+                    reset index
                 Columns
                     - Date (pandas.Timestamp): Observation date
                     - Confirmed (int): the number of confirmed cases
@@ -87,3 +89,47 @@ class PhaseTracker(Term):
                         - Runtime (str): runtime of optimization
         """
         return self._track_df.drop(self.ID, axis=1).reset_index()
+
+    def summary(self):
+        """
+        Summarize phase information.
+
+        Returns:
+            pandas.DataFrame
+                Index
+                    str: phase names
+                Columns
+                    - Type: 'Past' or 'Future'
+                    - Start: start date of the phase
+                    - End: end date of the phase
+                    - Population: population value of the start date
+                    - If available,
+                        - ODE (str): ODE model names
+                        - Rt (float): phase-dependent reproduction number
+                        - (str, float): estimated parameter values, including rho
+                        - tau (int): tau value [min]
+                        - (int or float): day parameters, including 1/beta [days]
+                        - {metric}: score with the estimated parameter values
+                        - Trials (int): the number of trials
+                        - Runtime (str): runtime of optimization
+        """
+        # Remove un-registered phase
+        track_df = self._track_df.reset_index()
+        track_df = track_df.loc[track_df[self.ID] != 0]
+        # -> index=phase names, columns=Start/variables,.../End
+        track_df[self.ID], _ = track_df[self.ID].factorize()
+        first_df = track_df.groupby(self.ID).first()
+        df = first_df.join(track_df.groupby(self.ID).last(), rsuffix="_last")
+        df = df.rename(columns={self.DATE: self.START, f"{self.DATE}_last": self.END})
+        df.index.name = None
+        df.index = [self.num2str(num) for num in df.index]
+        df = df.loc[:, [col for col in df.columns if "_last" not in col]]
+        # Calculate phase types: Past or Future
+        df[self.TENSE] = (df[self.START] <= self._today).map({True: self.PAST, False: self.FUTURE})
+        # Calculate population values
+        df[self.N] = df[[self.S, self.C]].sum(axis=1)
+        # Set the order of columns
+        df = df.drop([self.C, self.CI, self.F, self.R, self.S], axis=1)
+        fixed_cols = self.TENSE, self.START, self.END, self.N
+        others = [col for col in df.columns if col not in set(fixed_cols)]
+        return df.loc[:, [*fixed_cols, *others]]
