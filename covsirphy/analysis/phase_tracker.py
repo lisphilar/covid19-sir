9c6246a63cb4042e92899919d5f1fe482dad3b11
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2021-04-29 14:52:54 +0900

    add: PhaseTracker.trend()

--- covsirphy/analysis/phase_tracker.py
+++ covsirphy/analysis/phase_tracker.py
@@ -3,7 +3,10 @@
 
 from datetime import timedelta
 import pandas as pd
+from covsirphy.util.argument import find_args
 from covsirphy.util.term import Term
+from covsirphy.trend.trend_plot import trend_plot
+from covsirphy.trend.trend_detector import TrendDetector
 
 
 class PhaseTracker(Term):
@@ -22,11 +25,13 @@ class PhaseTracker(Term):
                 - Recovered (int): the number of recovered cases
                 - Susceptible (int): the number of susceptible cases
         today (str or pandas.Timestamp): reference date to determine whether a phase is a past phase or not
+        area (str): area name, like Japan/Tokyo
     """
 
-    def __init__(self, data, today):
+    def __init__(self, data, today, area):
         self._ensure_dataframe(data, name="data", columns=self.SUB_COLUMNS)
         self._today = self._ensure_date(today, name="today")
+        self._area = str(area)
         # Tracker of phase information: index=Date, records of C/I/F/R/S, phase ID (0: not defined)
         self._track_df = data.set_index(self.DATE)
         self._track_df[self.ID] = 0
@@ -149,3 +154,28 @@ class PhaseTracker(Term):
         fixed_cols = self.TENSE, self.START, self.END, self.N
         others = [col for col in df.columns if col not in set(fixed_cols)]
         return df.loc[:, [*fixed_cols, *others]]
+
+    def trend(self, force, show_figure, **kwargs):
+        """
+        Define past phases with S-R trend analysis.
+
+        Args:
+            force (bool): if True, change points will be over-written
+            show_figure (bool): if True, show the result as a figure
+            kwargs: keyword arguments of covsirphy.TrendDetector(), .TrendDetector.sr() and .trend_plot()
+
+        Returns:
+            covsirphy.PhaseTracker: self
+        """
+        df = self._track_df.loc[:self._today].reset_index()[self.SUB_COLUMNS]
+        detector = TrendDetector(data=df, area=self._area, **find_args(TrendDetector, **kwargs))
+        # Perform S-R trend analysis
+        detector.sr(**find_args(TrendDetector.sr, **kwargs))
+        # Register phases
+        if force:
+            start_dates, end_dates = detector.dates()
+            [self.define_phase(start, end) for (start, end) in zip(start_dates, end_dates)]
+        # Show S-R plane
+        if show_figure:
+            detector.show(**find_args(trend_plot, **kwargs))
+        return self
