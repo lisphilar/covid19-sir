b5019f423fc41b5012abab32aa27aaf3bcb01924
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2021-04-30 02:33:14 +0900

    fix: AttributeError

--- covsirphy/analysis/scenario.py
+++ covsirphy/analysis/scenario.py
@@ -475,11 +475,12 @@ class Scenario(Term):
         """
         tracker = self._tracker(name, template=template)
         if include_past:
-            self[name] = tracker.delete_all()
+            self[name] = tracker.remove_phase(self._data.first_date, self._data.last_date)
         else:
             df = tracker.summary()
             future_phases = df.loc[df[self.TENSE] == self.FUTURE].index.tolist()
-            self[name] = tracker.delete(phases=future_phases)
+            dates = tracker.phase_to_dates(phases=future_phases)
+            self[name] = tracker.remove_phase(min(dates), max(dates))
         return self
 
     def _delete_series(self, name):
