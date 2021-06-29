fcb1c7a03502e5c8bd6d5c1aa2959f23d6f1ce37
lisphilar <7270139+lisphilar@users.noreply.github.com>
2021-06-30 00:39:39 +0900

    test

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -65,15 +65,6 @@ class TestDataLoader(object):
 @pytest.mark.filterwarnings("ignore", category=DeprecationWarning)
 class TestCOVID19DataHub(object):
     def test_covid19dh(self):
-        with pytest.raises(TypeError):
-            COVID19DataHub(filename=None)
-        data_hub = COVID19DataHub(
-            filename=Path("input").joinpath("covid19dh.csv"))
-        # Citation (with downloading), disabled to avoid downloading many times
-        # assert isinstance(data_hub.primary, str)
-        # Retrieve the dataset from the server
-        data_hub.load(name="jhu", force=False, verbose=False)
-        with pytest.raises(KeyError):
-            data_hub.load(name="unknown")
-        # Citation (without downloading)
+        data_hub = COVID19DataHub(filename=Path("input").joinpath("covid19dh.csv"))
+        data_hub.load(name="jhu")
         assert isinstance(data_hub.primary, str)
