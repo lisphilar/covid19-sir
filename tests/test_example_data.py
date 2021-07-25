104867641f72de5a2b831dedcf2ab1905ed09c93
lisphilar <7270139+lisphilar@users.noreply.github.com>
2021-07-26 01:32:52 +0900

    test

--- tests/test_example_data.py
+++ tests/test_example_data.py
@@ -23,7 +23,7 @@ class TestExampleData(object):
             example_data.subset()
         # Cleaned dataset
         clean_df = example_data.cleaned()
-        cols = [Term.DATE, Term.COUNTRY, Term.PROVINCE, Term.C, Term.CI, Term.F, Term.R, Term.N]
+        cols = [Term.ISO3, Term.DATE, Term.COUNTRY, Term.PROVINCE, Term.C, Term.CI, Term.F, Term.R, Term.N]
         assert set(clean_df.columns) == set(cols)
         # Subset
         subset_df = example_data.subset(model=model)
