cd667948f39a4238604f9e5d2abfb83cc107c4a7
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2022-03-02 03:32:17 +0900

    test

--- tests/test_scenario.py
+++ tests/test_scenario.py
@@ -269,7 +269,7 @@ class TestScenario(object):
         assert "Multivariate_regression_Likely" not in snl._tracker_dict
         assert "Likely" in snl._tracker_dict
         # Delete all scenarios except Main/Likely scenario
-        with pytest.raises(KeyError):
+        with pytest.raises(ValueError):
             snl.represent(q=[0.05, 0.95], variable="Fatal", date=None, excluded=["Main", "Likely"])
         snl.adjust_end()
         best, worst = snl.represent(q=[0.05, 0.95], variable="Fatal", date=None, excluded=["Main", "Likely"])
