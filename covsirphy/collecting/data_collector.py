7865b05ee38eb52b78ec52d9858689c5bde248ad
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2022-04-17 01:44:01 +0900

    update: fillin DataCollector.manual()

--- covsirphy/collecting/data_collector.py
+++ covsirphy/collecting/data_collector.py
@@ -11,6 +11,7 @@ class DataCollector(Term):
 
     Args:
         directory (str or pathlib.Path): directory to save downloaded datasets
+        layers (list[str] or None): list of layers of geographic information or None (["Country", "Province", "City"])
         update_interval (int or None): update interval of downloading dataset or None (avoid downloading)
         basename_dict (dict[str, str]): basename of downloaded CSV files,
             "covid19dh": COVID-19 Data Hub (default: covid19dh.csv),
@@ -37,19 +38,8 @@ class DataCollector(Term):
         "google": "google_cloud_platform",
         "japan": "covid_japan",
     }
-    # Default values of layers
-    LAYERS = [Term.COUNTRY, Term.PROVINCE, Term.CITY]
-    # Default variables of the collected data
-    VARIABLES = [
-        # The number of cases
-        Term.C, Term.CI, Term.F, Term.R, Term.N,
-        # PCR tests
-        Term.TESTS, Term.TESTS_DIFF,
-        # Vaccinations
-        Term.VAC, Term.VAC_BOOSTERS, Term.V_ONCE, Term.V_FULL,
-    ]
 
-    def __init__(self, directory="input", update_interval=12, basename_dict=None, verbose=1):
+    def __init__(self, directory="input", layers=None, update_interval=12, basename_dict=None, verbose=1):
         # Filenames to save remote datasets
         filer = Filer(directory=directory, prefix=None, suffix=None, numbering=None)
         self._file_dict = {
@@ -60,20 +50,22 @@ class DataCollector(Term):
         # Verbosity
         self._verbose = self._ensure_natural_int(verbose, name="verbose", include_zero=True)
         # Location data
-        self._loc_df = pd.DataFrame(index=[self._ID])
+        self._layers = self._ensure_list(target=layers or [Term.COUNTRY, Term.PROVINCE, Term.CITY], name="layers")
+        self._loc_df = pd.DataFrame(columns=[self._ID, *self._layers])
         # All available data
-        self._all_df = pd.DataFrame(index=[self._ID, self.DATE])
+        self._rec_df = pd.DataFrame(columns=[self._ID, self.DATE])
         # Citations
-        self._citations = []
+        self._citation_dict = {}
 
-    @property
-    def citations(self):
+    def citations(self, variables=None):
         """
-        str: citation list of the secondary data sources
+        Return citation list of the secondary data sources.
+        variables (list[str] or None): list of variables to collect or None (all available variables)
         """
-        return self._citations
+        columns = self._ensure_list(target=variables or None, name="variables")
+        return self.flatten([v for (k, v) in self._citation_dict.items() if k in columns], unique=True)
 
-    def manual(self, data, layers=None, date="Date", dayfirst=False):
+    def manual(self, data, date="Date", variables=None, citations=None, **kwargs):
         """Add data manually.
 
         Args:
@@ -81,29 +73,42 @@ class DataCollector(Term):
                 Index
                     reset index
                 Columns
+                    - columns defined by covsirphy.DataCollector(layers)
                     - columns defined by @date
-                    - columns defined by @layers
-                    - all remained columns will be regarded as records
-            layers (list[str] or None): list of layers of geographic information or None (["Country", "Province", "City"])
+                    - columns defined by @variables
             date (str): column name of date
-            dayfirst (bool): whether date format is DD/MM or not
+            variables (list[str] or None): list of variables to add or None (all available columns)
+            citations (list[str] or None): citations of the dataset or None (["my own dataset"])
+            **kwargs: keyword arguments of pandas.to_datetime() including "dayfirst (bool): whether date format is DD/MM or not"
 
         Returns:
             covsirphy.DataCollector: self
         """
-        pass
+        df = self._ensure_dataframe(target=data, name="data", columns=[*self._layers, date])
+        df.rename(columns={date: self.DATE}, inplace=True)
+        df[self.DATE] = pd.to_datetime(df[self.DATE], **kwargs)
+        # Locations
+        loc_df = self._loc_df.merge(df, how="left", on=self._layers, ignore_index=True)
+        loc_df.loc[loc_df[self._ID].isna(), self._ID] = "id" + loc_df[loc_df[self._ID].isna()].index.astype("str")
+        self._loc_df = loc_df.reset_index()[[self._ID, *self._layers]]
+        # Records
+        columns = [self._ID, self.DATE, *self._ensure_list(target=variables or None, name=variables)]
+        df = df.merge(self._loc_df, how="left", on=self._layers)
+        df = df.loc[:, columns].reset_index(drop=True)
+        self._rec_df = pd.concat([self._rec_df, df], axis=0, ignore_index=True)
+        # Citations
+        citation_dict = {col: self._citation_dict.get(col, []) + (citations or "my own dataset") for col in variables}
+        self._citation_dict.update(citation_dict)
+        return self
 
     def collect(self, geo=None, variables=None):
         """Collect necessary data from remote server and local data.
 
         Args:
             geo (tuple(list[str] or tuple(str) or str)): location names defined in covsirphy.Geography class
-            variables (list[str] or None): list of variables to collect or None (default values)
+            variables (list[str] or None): list of variables to collect or None (all available variables)
 
         Note:
             Please refer to covsirphy.Geography.filter() regarding @geo argument.
-
-        Note:
-            Default variables are defined by covsirphy.DataCollector.VARIABLES (class variable).
         """
         pass
