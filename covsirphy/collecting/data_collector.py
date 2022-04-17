32dd3c7c2b0e9dee5b1cbe575703117ad22e6dd4
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2022-04-17 23:37:18 +0900

    update: auto downloader from Data Hub

--- covsirphy/collecting/data_collector.py
+++ covsirphy/collecting/data_collector.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from itertools import zip_longest
 import country_converter as coco
 import pandas as pd
 from covsirphy.util.term import Term
@@ -14,17 +15,30 @@ class DataCollector(Term):
     Args:
         layers (list[str] or None): list of layers of geographic information or None (["ISO3", "Province", "City"])
         country (str or None): layer name of countries or None (countries are not included in the layers)
+        update_interval (int): update interval of downloading dataset
+        verbose (int): level of verbosity when downloading
 
     Note:
         Country level data specified with @country will be stored with ISO3 codes.
+
+    Note:
+        If @update_interval hours have passed since the last update of downloaded datasets,
+        the dawnloaded datasets will be updated automatically.
+
+    Note:
+        If @verbose is 0, no description will be shown.
+        If @verbose is 1 or larger, URL and database name will be shown.
+        If @verbose is 2, detailed citation list will be show, if available.
     """
     _ID = "Location_ID"
 
-    def __init__(self, layers=None, country="ISO3"):
+    def __init__(self, layers=None, country="ISO3", update_interval=12, verbose=1):
+        self._update_interval = self._ensure_natural_int(update_interval, name="update_interval", include_zero=True)
+        self._verbose = self._ensure_natural_int(verbose, name="verbose", include_zero=True)
         # Countries will be specified with ISO3 codes and this requires conversion
         self._country = None if country is None else str(country)
         # Location data
-        self._layers = self._ensure_list(target=layers or [self._country, Term.PROVINCE, Term.CITY], name="layers")
+        self._layers = self._ensure_list(target=layers or [self._country, self.PROVINCE, self.CITY], name="layers")
         self._loc_df = pd.DataFrame(columns=[self._ID, *self._layers])
         # All available data
         self._rec_df = pd.DataFrame(columns=[self._ID, self.DATE])
@@ -153,34 +167,21 @@ class DataCollector(Term):
         df = geography.filter(data=all_df, geo=geo_converted)
         return df.drop(self._layers, axis=1).groupby(self.DATE).sum().reset_index()
 
-    def auto(self, iso3=None, directory="input", update_interval=12, basename_dict=None, verbose=1):
+    def auto(self, iso3=None, directory="input", basename_dict=None):
         """Download datasets from remote servers automatically.
 
         Args:
             iso3 (str or None): ISO3 code of country that must be included or None (all available countries)
             directory (str or pathlib.Path): directory to save downloaded datasets
-            update_interval (int or None): update interval of downloading dataset or None (avoid downloading)
             basename_dict (dict[str, str]): basename of downloaded CSV files,
                 "covid19dh": COVID-19 Data Hub (default: covid19dh.csv),
                 "owid": Our World In Data (default: ourworldindata.csv),
                 "google: COVID-19 Open Data by Google Cloud Platform (default: google_cloud_platform.csv),
                 "japan": COVID-19 Dataset in Japan (default: covid_japan.csv).
-            verbose (int): level of verbosity when downloading
 
         Returns:
             covsirphy.DataCollector: self
-
-        Note:
-            If @update_interval (not None) hours have passed since the last update of downloaded datasets,
-            the dawnloaded datasets will be updated automatically.
-
-        Note:
-            If @verbose is 0, no description will be shown.
-            If @verbose is 1 or larger, URL and database name will be shown.
-            If @verbose is 2, detailed citation list will be show, if available.
         """
-        self._ensure_natural_int(update_interval, name="update_interval", include_zero=True, none_ok=True)
-        self._ensure_natural_int(verbose, name="verbose", include_zero=True)
         # Filenames to save remote datasets
         TITLE_DICT = {
             "covid19dh": "covid19dh",
@@ -192,11 +193,69 @@ class DataCollector(Term):
         file_dict = {
             k: filer.csv(title=(basename_dict or {}).get(k, v))["path_or_buf"] for (k, v) in TITLE_DICT.items()}
         # COVID-19 Data Hub
-        self.manual(data=self._auto_covid19dh(iso3=iso3, local_file=file_dict["covid19dh"]), date="date")
+        self.manual(**self._auto_covid19dh(iso3=iso3, path=file_dict["covid19dh"]))
         # Our World In Data
         # Google Cloud Plat Form
         # Japan dataset via CovsirPhy project
         return self
 
-    def _auto_covid19dh(**kwargs):
-        pass
+    def _auto_covid19dh(self, iso3, path):
+        """Prepare records of the number of confirmed/fatal/recovered cases, the number of PCR tests and OXCGRT indicators.
+
+        Args:
+            iso3 (str or None): ISO3 code of country that must be included or None (all available countries)
+            path (str): path of the CSV file to save dataset in local environment
+
+        Returns:
+            dict[str, pandas.DataFrame or str or list[str]]]
+                - data (pandas.DataFrame):
+                    Index
+                        reset index
+                    Columns
+                        - (str): layers defined by DataCollector(layers) argument
+                        - date (pandas.Timestamp): observation dates
+                        - Confirmed (numpy.int64): the number of confirmed cases
+                        - Fatal (numpy.int64): the number of fatal cases
+                        - Recovered (numpy.int64): the number of recovered cases
+                        - Tests (numpy.int64): the number of PCR tests
+                        - School_closing (numpy.int64): one of the OxCGRT indicator
+                        - Workplace_closing (numpy.int64): one of the OxCGRT indicator
+                        - Cancel_events (numpy.int64): one of the OxCGRT indicator
+                        - Gatherings_restrictions (numpy.int64): one of the OxCGRT indicator
+                        - Transport_closing (numpy.int64): one of the OxCGRT indicator
+                        - Stay_home_restrictions (numpy.int64): one of the OxCGRT indicator
+                        - Internal_movement_restrictions (numpy.int64): one of the OxCGRT indicator
+                        - International_movement_restrictions (numpy.int64): one of the OxCGRT indicator
+                        - Information_campaigns (numpy.int64): one of the OxCGRT indicator
+                        - Testing_policy (numpy.int64): one of the OxCGRT indicator
+                        - Contact_tracing (numpy.int64): one of the OxCGRT indicator
+                        - Stringency_index (numpy.int64): one of the OxCGRT indicator
+                - citations (list[str]): citation of COVID-19 Data Hub
+        """
+        col_dict = {
+            "date": self.DATE, "iso_alpha_3": self.ISO3,
+            "confirmed": self.C, "deaths": self.F, "recovered": self.R, "tests": self.TESTS, "population": self.N,
+        }
+        # Get raw data from server
+        if iso3 is None:
+            url = "https://storage.covid19datahub.io/level/1.csv.zip"
+        else:
+            url = f"https://storage.covid19datahub.io/country/{iso3}.csv.zip"
+        df = pd.read_csv(
+            url, header=0, use_cols=list(col_dict.values()), parse_dates="date", date_parser=lambda x: pd.datetime.strptime(x, "%Y-%m-%d"))
+        df.rename(columns=col_dict, inplace=True)
+        # Perform sequence alignment with layers
+        dh_layers = [self.ISO3, "administrative_area_level_2", "administrative_area_level_3"]
+        if self._country == self.ISO3:
+            defined_lower_layers = self._layers[self._layers.index(self.ISO3) + 1:]
+            for (admin_level, layer) in zip_longest([self.PROVINCE, self.CITY], defined_lower_layers):
+                if admin_level is None:
+                    break
+                if layer is None:
+                    df = df.loc[df[admin_level].isna()]
+                    break
+                df.rename(columns={admin_level: layer}, inplace=True)
+        else:
+            df.rename(columns={admin_level: layer for (admin_level, layer) in zip_longest()}, inplace=True)
+        for layer in self._layers:
+            df.loc[:, layer] = df[layer].fillna(self.NA)
