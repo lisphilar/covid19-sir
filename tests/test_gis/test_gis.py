aa94ba3a5185ada4ef47fc4caa835f949de77cbe
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2022-05-02 23:11:45 +0900

    test

--- tests/test_gis/test_gis.py
+++ tests/test_gis/test_gis.py
@@ -42,8 +42,34 @@ class TestGIS(object):
         [
             (None, "31Dec2021", 365),
             ((None,), "31Dec2021", 365),
-            ("Japan", "31Dec2021", 365),
             ("Japan", "31Dec2020", 0),
+            ("Japan", "31Dec2021", 365 * 47),
+            (("Japan",), "31Dec2021", 365 * 47),
+            ((["Japan", "UK"],), "31Dec2021", 365 * 47),
+            ("UK", "31Dec2021", 0),
+        ]
+    )
+    def test_layer(self, c_df, p_df, geo, end_date, length):
+        system = GIS(layers=["Country", "Province"], country="Country", date="Date")
+        with pytest.raises(NotRegisteredError):
+            system.layer(geo=geo, start_date="01Jan2021", end_date=end_date, variables=None)
+        system.register(data=c_df, layers=["Country"], date="date", citations="Country-level")
+        system.register(data=p_df, layers=["Country", "Prefecture"], date="date", citations="Prefecture-level")
+        if length == 0:
+            with pytest.raises(NotRegisteredError):
+                system.layer(geo=geo, start_date="01Jan2021", end_date=end_date, variables=None)
+        else:
+            df = system.layer(geo=geo, start_date="01Jan2021", end_date=end_date, variables=None)
+            assert df.columns.tolist() == ["Country", "Province", "Date", "Positive", "Tested"]
+            assert len(df) == length
+
+    @pytest.mark.parametrize(
+        "geo, end_date, length",
+        [
+            (None, "31Dec2021", 365),
+            ((None,), "31Dec2021", 365),
+            ("Japan", "31Dec2020", 0),
+            ("Japan", "31Dec2021", 365),
             (("Japan",), "31Dec2021", 365),
             ((["Japan", "UK"],), "31Dec2021", 365),
             ("UK", "31Dec2021", 0),
