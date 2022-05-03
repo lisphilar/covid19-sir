f32afc797e4bad74c9917edeb2a329f63eb0d5c4
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2022-05-03 19:03:19 +0900

    fix: #1059

--- covsirphy/cleaning/cbase.py
+++ covsirphy/cleaning/cbase.py
@@ -249,11 +249,11 @@ class CleaningBase(Term):
 
         Note:
             If province is None or '-', return country name.
-            If not, return the area name, like 'Japan/Tokyo'
+            If not, return the area name, like 'Tokyo/Japan'
         """
         if province in [None, cls.NA]:
             return country
-        return f"{country}{cls.SEP}{province}"
+        return f"{province}{cls.SEP}{country}"
 
     def layer(self, country=None):
         """
