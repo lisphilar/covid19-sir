164c2430d7b38751920cecb9ff306b7638e628a7
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2022-05-05 18:40:19 +0900

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,9 @@
 
 from pathlib import Path
 import warnings
+import pandas as pd
 import pytest
-from covsirphy import DataLoader
+from covsirphy import DataLoader, JapanData
 
 
 @pytest.fixture(scope="session")
@@ -75,3 +76,29 @@ def imgfile():
         # Python 3.7
         if filepath.exists():
             filepath.unlink()
+
+
+@pytest.fixture(scope="session")
+def c_df():
+    path = Path("data", "japan", "covid_jpn_total.csv")
+    filepath = path if path.exists() else JapanData.URL_C
+    df = pd.read_csv(filepath, dayfirst=False).rename(columns={"Date": "date"})
+    df = df[["date", "Positive", "Tested", "Discharged", "Fatal"]].groupby("date", as_index=False).sum()
+    df.insert(0, "Country", "Japan")
+    return df
+
+
+@pytest.fixture(scope="session")
+def p_df():
+    path = Path("data", "japan", "covid_jpn_prefecture.csv")
+    filepath = path if path.exists() else JapanData.URL_P
+    df = pd.read_csv(filepath, dayfirst=False).rename(columns={"Date": "date"})
+    df.insert(0, "Country", "Japan")
+    return df[["Country", "Prefecture", "date", "Positive", "Tested", "Discharged", "Fatal"]]
+
+
+@pytest.fixture(scope="session")
+def japan_df(c_df, p_df):
+    df = pd.concat([c_df, p_df], axis=0)
+    assert set(df.columns) == {"Country", "Prefecture", "date", "Positive", "Tested", "Discharged", "Fatal"}
+    return df
