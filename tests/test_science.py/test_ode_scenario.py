2a10de789b90f34ed8171ee4848eb635d9b5be16
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2022-06-12 16:53:24 +0900

    test

--- tests/test_science.py/test_ode_scenario.py
+++ tests/test_science.py/test_ode_scenario.py
@@ -7,6 +7,9 @@ from covsirphy import ODEScenario, SIRFModel, SubsetNotFoundError
 
 class TestODEScenario(object):
     def test_auto_build(self):
-        ODEScenario.auto_build(geo="Japan", model=SIRFModel)
+        snr = ODEScenario.auto_build(geo="Japan", model=SIRFModel)
         with pytest.raises(SubsetNotFoundError):
             ODEScenario.auto_build(geo="Moon", model=SIRFModel)
+        snr.summary()
+        snr.simulate()
+        snr.simulate(name="Baseline")
