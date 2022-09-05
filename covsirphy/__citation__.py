317f9c7f5f17a0fd6854c48b21ace00cf4fe5082
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2022-09-06 00:20:48 +0900

    new: close #1164

--- covsirphy/__citation__.py
+++ covsirphy/__citation__.py
@@ -4,6 +4,5 @@
 from covsirphy.__version__ import __version__
 
 __citation__ = f"""
-CovsirPhy Development Team (2020-2022), CovsirPhy version {__version__}: Python library for COVID-19 analysis with phase-dependent SIR-derived ODE models, https://github.com/lisphilar/covid19-sir
-Hirokazu Takaya (2020-2022), Kaggle Notebook, COVID-19 data with SIR model, https://www.kaggle.com/lisphilar/covid-19-data-with-sir-model
+Hirokazu Takaya and CovsirPhy Development Team (2020-2022), CovsirPhy version {__version__}: Python library for COVID-19 analysis with phase-dependent SIR-derived ODE models, https://github.com/lisphilar/covid19-sir
 """
