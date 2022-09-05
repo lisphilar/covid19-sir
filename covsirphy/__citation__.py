4609a31c481d0e5873a73a3208907495a85ebea4
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2022-09-06 00:24:50 +0900

    fix: remove \n, #1164

--- covsirphy/__citation__.py
+++ covsirphy/__citation__.py
@@ -3,6 +3,7 @@
 
 from covsirphy.__version__ import __version__
 
-__citation__ = f"""
-Hirokazu Takaya and CovsirPhy Development Team (2020-2022), CovsirPhy version {__version__}: Python library for COVID-19 analysis with phase-dependent SIR-derived ODE models, https://github.com/lisphilar/covid19-sir
-"""
+__citation__ = "Hirokazu Takaya and CovsirPhy Development Team (2020-2022), " \
+    f"CovsirPhy version {__version__}: " \
+    "Python library for COVID-19 analysis with phase-dependent SIR-derived ODE models, " \
+    "https://github.com/lisphilar/covid19-sir"
