e5aee333625f220775183930c5c4cf5d16c7695e
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2022-09-25 00:11:16 +0900

    fix: FutureWarning: In a future version, `df.iloc[:, i] = newvals` will attempt to set the values inplace instead of always setting a new array

--- covsirphy/dynamics/dynamics.py
+++ covsirphy/dynamics/dynamics.py
@@ -171,10 +171,9 @@ class Dynamics(Term):
             NA can used in the newer phases because filled with that of the older phases.
         """
         if data is not None:
-            all_df = self._df.copy()
             new_df = Validator(data, "data").dataframe(time_index=True)
             new_df.index = pd.to_datetime(new_df.index).round("D")
-            all_df[:] = np.nan
+            all_df = pd.DataFrame(np.nan, index=self._df.index, columns=self._df.columns)
             all_df[self._PH] = 0
             all_df.update(new_df, overwrite=True)
             if all_df.loc[self._first, self._SIRF].isna().any():
