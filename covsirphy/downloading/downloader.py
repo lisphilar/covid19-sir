6a8af0158b465ce31a4ef22843d3732c2f67241e
lisphilar <7270139+lisphilar@users.noreply.github.com>
2022-10-15 02:37:05 +0900

    update docstrings

--- covsirphy/downloading/downloader.py
+++ covsirphy/downloading/downloader.py
@@ -19,7 +19,7 @@ class DataDownloader(Term):
         update_interval: update interval of downloading dataset
 
     Note:
-        Location layers are fixed to ["ISO3", "Province", "City"]
+        Location layers are fixed to ['ISO3', 'Province', 'City'].
     """
     LAYERS = [Term.ISO3, Term.PROVINCE, Term.CITY]
 
@@ -35,54 +35,50 @@ class DataDownloader(Term):
             country: country name or None
             province: province/state/prefecture name or None
             databases: databases to use or None (japan, covid19dh, owid).
-                "japan": COVID-19 Dataset in Japan,
-                "covid19dh": COVID-19 Data Hub,
-                "owid": Our World In Data,
-                "wpp": World Population Prospects by United nations.
+                Candidates are as follows.
+
+                - "japan": COVID-19 Dataset in Japan,
+                - "covid19dh": COVID-19 Data Hub,
+                - "owid": Our World In Data,
+                - "wpp": World Population Prospects by United nations.
 
         Returns:
-            dataframe:
-                Index
-                    reset index
-                Columns
-                    Date (pandas.Timestamp): observation date
-                    ISO3 (str): country names
-                    Province (str): province/state/prefecture names
-                    City (str): city names
-                    Country (str): country names (top level administration)
-                    Province (str): province names (2nd level administration)
-                    ISO3 (str): ISO3 codes
-                    Confirmed (pandas.Int64): the number of confirmed cases
-                    Fatal (pandas.Int64): the number of fatal cases
-                    Recovered (pandas.Int64): the number of recovered cases
-                    Population (pandas.Int64): population values
-                    Tests (pandas.Int64): the number of tests
-                    Product (pandas.Int64): vaccine product names
-                    Vaccinations (pandas.Int64): cumulative number of vaccinations
-                    Vaccinations_boosters (pandas.Int64): cumulative number of booster vaccinations
-                    Vaccinated_once (pandas.Int64): cumulative number of people who received at least one vaccine dose
-                    Vaccinated_full (pandas.Int64): cumulative number of people who received all doses prescribed by the protocol
-                    School_closing
-                    Workplace_closing
-                    Cancel_events
-                    Gatherings_restrictions
-                    Transport_closing
-                    Stay_home_restrictions
-                    Internal_movement_restrictions
-                    International_movement_restrictions
-                    Information_campaigns
-                    Testing_policy
-                    Contact_tracing
-                    Stringency_index
+            A dataframe with reset index and the following columns.
+
+                - Date (pandas.Timestamp): observation date
+                - ISO3 (str): country names
+                - Province (str): province/state/prefecture names
+                - City (str): city names
+                - Country (str): country names (the top level administration)
+                - Province (str): province names (the 2nd level administration)
+                - ISO3 (str): ISO3 codes
+                - Confirmed (pandas.Int64): the number of confirmed cases
+                - Fatal (pandas.Int64): the number of fatal cases
+                - Recovered (pandas.Int64): the number of recovered cases
+                - Population (pandas.Int64): population values
+                - Tests (pandas.Int64): the number of tests
+                - Product (pandas.Int64): vaccine product names
+                - Vaccinations (pandas.Int64): cumulative number of vaccinations
+                - Vaccinations_boosters (pandas.Int64): cumulative number of booster vaccinations
+                - Vaccinated_once (pandas.Int64): cumulative number of people who received at least one vaccine dose
+                - Vaccinated_full (pandas.Int64): cumulative number of people who received all doses prescribed by the protocol
+                - School_closing
+                - Workplace_closing
+                - Cancel_events
+                - Gatherings_restrictions
+                - Transport_closing
+                - Stay_home_restrictions
+                - Internal_movement_restrictions
+                - International_movement_restrictions
+                - Information_campaigns
+                - Testing_policy
+                - Contact_tracing
+                - Stringency_index
 
         Note:
-            When @country is None, country-level data will be returned.
-
-        Note:
-            When @country is a string and @province is None, province-level data in the country will be returned.
-
-        Note:
-            When @country and @province are strings, city-level data in the province will be returned.
+            - When @country is None, country-level data will be returned.
+            - When @country is a string and @province is None, province-level data in the country will be returned.
+            - When @country and @province are strings, city-level data in the province will be returned.
         """
         db_dict = {
             "japan": _CSJapan,
