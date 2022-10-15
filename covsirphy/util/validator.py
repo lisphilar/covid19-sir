4bab08a1b136f222ecdbaa68a3ab4dbf5bb02412
lisphilar <7270139+lisphilar@users.noreply.github.com>
2022-10-15 21:12:23 +0900

    fix ImportError of Callable

--- covsirphy/util/validator.py
+++ covsirphy/util/validator.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 from inspect import signature
 import math
 import pandas as pd
-from typing_extensions import Any, Callable, Iterable
+from typing import Callable
+from typing_extensions import Any, Iterable
 from covsirphy.util.error import NAFoundError, NotIncludedError, NotSubclassError, UnExpectedTypeError, EmptyError
 from covsirphy.util.error import UnExpectedValueRangeError, UnExpectedValueError, UnExpectedLengthError, UnExpectedNoneError
 
