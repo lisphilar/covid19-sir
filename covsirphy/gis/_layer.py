d36a6b2b703bdb85a71d609090bbcbc9c66a89b6
Lisphilar <7270139+lisphilar@users.noreply.github.com>
2023-02-04 15:30:37 +0900

    close #1350

--- covsirphy/gis/_layer.py
+++ covsirphy/gis/_layer.py
@@ -112,7 +112,7 @@ class _LayerAdjuster(Term):
             df[self.DATE] = df[self.DATE].dt.tz_convert(None)
         # Convert country names to ISO3 codes
         if convert_iso3 and self._country is not None and self._country in df:
-            df.loc[:, self._country] = self._to_iso3(df[self._country])
+            df.isetitem(self._country, self._to_iso3(df[self._country]))
         # Prepare necessary layers and fill in None with "NA"
         if data_layers is not None and data_layers != self._layers:
             df = self._prepare_layers(df, data_layers=data_layers)
