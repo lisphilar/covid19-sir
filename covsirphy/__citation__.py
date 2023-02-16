0034a1855b2b95c810ab7f788fbc0d559aa777ea
mehrdadbn9 <80095851+mehrdadbn9@users.noreply.github.com>
2023-02-16 12:14:14 +0330

    Update __citation__.py

--- covsirphy/__citation__.py
+++ covsirphy/__citation__.py
@@ -1,6 +1,6 @@
 from covsirphy.__version__ import __version__
 
-__citation__ = "Hirokazu Takaya and CovsirPhy Development Team (2020-2022), " \
+__citation__ = "Hirokazu Takaya and CovsirPhy Development Team (2020-2023), " \
     f"CovsirPhy version {__version__}: " \
     "Python library for COVID-19 analysis with phase-dependent SIR-derived ODE models, " \
     "https://github.com/lisphilar/covid19-sir"
