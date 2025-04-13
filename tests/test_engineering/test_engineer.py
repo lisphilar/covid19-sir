47e67dd5542bcc59a3bc816a3a6f78f88a72c3f5
Hirokazu Takaya <7270139+lisphilar@users.noreply.github.com>
2025-04-13 09:37:52 +0000

    test

--- tests/test_engineering/test_engineer.py
+++ tests/test_engineering/test_engineer.py
@@ -72,7 +72,7 @@ def test_variables_alias():
 def test_resample_with_date_range():
     eng = DataEngineer(layers=["Country"], country=["Country"])
     eng.download(databases=["wpp"])
-    eng.clean(kinds=["resample"], date_range=("01Jan2024", "19Sep2024"))
+    eng.clean(kinds=["resample"], date_range=("01Jan2021", "12Dec2024"))
     df = eng.all()
-    assert df["Date"].min() >= pd.to_datetime("01Jan2024")
-    assert df["Date"].max() <= pd.to_datetime("19Sep2024")
+    assert df["Date"].min() >= pd.to_datetime("01Jan2021")
+    assert df["Date"].max() <= pd.to_datetime("12Dec2024")
